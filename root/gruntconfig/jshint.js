3d27e7cfe44398f8a6f9c442c492a4521b2b4e4b
sato <sato@tsumikiinc.com>
2014-07-08 19:15:28 +0900

    fix #5
    jshint.js修正

--- root/gruntconfig/jshint.js
+++ root/gruntconfig/jshint.js
@@ -5,20 +5,14 @@ module.exports = {
   },
   {% if (addMobileSite === 'true') { %}
   pc: {
-    files: {
-      src: '<%= dir.src %>/<%= dir.pc %>/<%= dir.root %>/<%= dir.js %>/*.js'
-    }
+    src: '<%= dir.src %>/<%= dir.pc %>/<%= dir.root %>/<%= dir.js %>/*.js'
   },
   sp: {
-    files: {
-      src: '<%= dir.src %>/<%= dir.sp %>/<%= dir.root %>/<%= dir.js %>/*.js'
-    }
+    src: '<%= dir.src %>/<%= dir.sp %>/<%= dir.root %>/<%= dir.js %>/*.js'
   }
   {% } else { %}
   lint: {
-    files: {
-      src: '<%= dir.src %>/<%= dir.root %>/<%= dir.js %>/*.js'
-    }
+    src: '<%= dir.src %>/<%= dir.root %>/<%= dir.js %>/*.js'
   }
   {% } %}
 };
